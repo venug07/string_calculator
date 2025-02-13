a7f539dd75d3ce18aba9af47afde9d872f7cefbe
venug07 <venug078175@yahoo.com>
2025-02-13 19:06:01 +0530

    Functionality added for checking alphabets and calculator object added as common for all test cases

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -15,20 +15,26 @@ class StringCalculator
     end
     sepval.each { |seprt| string_numbers = string_numbers.gsub seprt, ","}
     @stringnumbers = string_numbers.split ","
-    neg_values = []
-    if !@stringnumbers.empty?
-      @stringnumbers.each do |sn|
+     neg_values = []
+     alph_values = []
+     if !@stringnumbers.empty?
+       @stringnumbers.each do |sn|
+        alph_values << sn if sn.match?(/[A-Za-z]/)
         neg_values << sn.to_i if sn.to_i < 0
-      end
-      if neg_values.empty?
-       if @stringnumbers.length > 1
-        @stringnumbers = @stringnumbers.inject {|acu, v| acu.to_i + v.to_i }
-       else
-        @stringnumbers.first.to_i
        end
-      else
-       raise Exception.new "Negative numbers not allowed " + neg_values.join(", ")
-      end
+       if alph_values.empty?
+        if neg_values.empty?
+         if @stringnumbers.length > 1
+          @stringnumbers = @stringnumbers.inject {|acu, v| acu.to_i + v.to_i }
+         else
+          @stringnumbers.first.to_i
+         end
+        else
+         raise Exception.new "Negative numbers not allowed " + neg_values.join(", ")
+        end
+       else
+        raise Exception.new "Alphabets not allowed " + alph_values.join(", ")
+       end
     else
       return 0
     end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,56 +3,51 @@ require './lib/string_calculator.rb'
 
 describe StringCalculator do
 
+  before :each do
+    @calculator = StringCalculator.new
+  end
+
   context "Given two digits" do 
     it "add the digits" do
-      calculator = StringCalculator.new
-      digitsum = calculator.add("1,2")
+      digitsum = @calculator.add("1,2")
       expect(digitsum).to eql(3)
     end
 
     it "add other digits" do
-      calculator = StringCalculator.new
-      digitsum = calculator.add("4,5")
+      digitsum = @calculator.add("4,5")
       expect(digitsum).to eql(9)
     end
 
     it "should raise the exception for negative values" do
-      calculator = StringCalculator.new
-      expect { calculator.add("-2,-3,1")}.to raise_exception {|exp| exp.message.should eql "Negative numbers not allowed -2, -3" }
+      expect { @calculator.add("-2,-3,1")}.to raise_exception {|exp| exp.message.should eql "Negative numbers not allowed -2, -3" }
     end
 
     it "For delimeter like //;\n1;2 should return 3" do
-      calculator = StringCalculator.new
-      calculator.add("//;\n1;2").should eql 3
+      @calculator.add("//;\n1;2").should eql 3
     end
 
     it "should return same value if there is only single value in string" do
-      calculator = StringCalculator.new
-      digitsum = calculator.add("5")
+      digitsum = @calculator.add("5")
       digitsum.should eql 5
     end
 
     it "should return 0 if string is empty" do
-      calculator = StringCalculator.new
-      digitsum = calculator.add("")
+      digitsum = @calculator.add("")
       digitsum.should eql 0
     end
 
     it "should return the correct output for multiple delimeters" do
-      calculator = StringCalculator.new
-      digitsum = calculator.add("//[***][---]\n1***6***7---3")
+      digitsum = @calculator.add("//[***][---]\n1***6***7---3")
       digitsum.should eql 17
     end
 
     it "should return the correct output for new line between numbers" do
-      calculator = StringCalculator.new
-      digitsum = calculator.add("1\n2,3")
+      digitsum = @calculator.add("1\n2,3")
       digitsum.should eql 6
     end
 
     it "should throw exception for alphabets" do
-      calculator = StringCalculator.new
-      expect { calculator.add("a,b,3")}.to raise_exception {|exp| exp.message.should eql "Alphabets not allowed a, b" }
+      expect { @calculator.add("a,b,3")}.to raise_exception {|exp| exp.message.should eql "Alphabets not allowed a, b" }
     end
   end
 
