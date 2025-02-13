e95cdad6671ef61cb04b04f715a9896bba25420b
venug07 <venug078175@yahoo.com>
2025-02-13 18:13:46 +0530

    Test case added for single value and empty string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -8,7 +8,7 @@ class StringCalculator
       neg_values << sn.to_i if sn.to_i < 0
     end
     if neg_values.empty?
-       true
+       @stringnumbers = @stringnumbers.inject {|acu, v| acu.to_i + v.to_i }
     else
       raise Exception.new "Negative numbers not allowed " + neg_values.join(", ")
     end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,9 +21,21 @@ describe StringCalculator do
       expect { calculator.add("-2,-3,1")}.to raise_exception {|exp| exp.message.should eql "Negative numbers not allowed -2, -3" }
     end
 
-     it "For delimeter like //;\n1;2 should return 3" do
+    it "For delimeter like //;\n1;2 should return 3" do
       calculator = StringCalculator.new
       calculator.add("//;\n1;2").should eql 3
+    end
+
+    it "should return same value if there is only single value in string" do
+      calculator = StringCalculator.new
+      digitsum = calculator.add("5")
+      digitsum.should eql 5
+    end
+
+    it "should return 0 if string is empty" do
+      calculator = StringCalculator.new
+      digitsum = calculator.add("")
+      digitsum.should eql 0
     end
   end
 
