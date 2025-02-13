cc65dab1d3d4232b1df45106bb488b5c36602084
venug07 <venug078175@yahoo.com>
2025-02-13 18:27:21 +0530

    Functionality added for single value and empty string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,16 +1,24 @@
 class StringCalculator
 
 
- def add(string_numbers)
+ def add(string_numbers="")
     neg_values = []
-    @stringnumbers = string_numbers.split(',')
-    @stringnumbers.each do |sn|
-      neg_values << sn.to_i if sn.to_i < 0
-    end
-    if neg_values.empty?
-       @stringnumbers = @stringnumbers.inject {|acu, v| acu.to_i + v.to_i }
+    if !string_numbers.empty?
+      @stringnumbers = string_numbers.split(',')
+      @stringnumbers.each do |sn|
+        neg_values << sn.to_i if sn.to_i < 0
+      end
+      if neg_values.empty?
+       if @stringnumbers.length > 1
+        @stringnumbers = @stringnumbers.inject {|acu, v| acu.to_i + v.to_i }
+       else
+        @stringnumbers.first.to_i
+       end
+      else
+       raise Exception.new "Negative numbers not allowed " + neg_values.join(", ")
+      end
     else
-      raise Exception.new "Negative numbers not allowed " + neg_values.join(", ")
+      return 0
     end
  end
 
